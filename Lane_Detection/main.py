c1ecb2f24bfad0d3e6ecefacfaa6110e4e1d8e9d
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-03-07 17:34:44 +0100

    Add the first suggested solution for lane detection

---
+++ Lane_Detection/main.py
@@ -0,0 +1,149 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+import utils
+
+
+video_name = "white.mp4"
+video_path = f"C:\\Users\\Choaib ELMADI\\Downloads\\D.I.F.Y\\Electronics\\Robotics\\7- Mini Tesla\\Lane Detection\\Videos\\{ video_name }"
+# video_path = "http://192.168.251.202:81/stream"
+
+global curves_list
+curves_list = []
+max_curves_list_len = 10
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+text_background = (198, 63, 88)
+corner_color = (53, 53, 249)
+text_color = (239, 239, 239)
+border_color = (61, 147, 8)
+
+
+def get_image_curve(image, show=2):
+    global curves_list
+    image_copy = np.copy(image)
+
+    thresholded_image = utils.threshold_image(image)
+
+    height, width, _ = image.shape
+    points = np.float32([[100, 180], [400, 180], [width - 10, height - 10], [10, height - 10]])  # type: ignore
+    warped_image = utils.warp_image(thresholded_image, points, width, height)
+
+    image_with_points = utils.draw_points(np.copy(image), points)
+
+    mid_point, _ = utils.pixel_summation(warped_image, percent=0.5, show=True, region=2)  # type: ignore
+    base_point, summation_image = utils.pixel_summation(warped_image, percent=0.9, show=True)  # type: ignore
+    raw_curve = base_point - mid_point
+
+    curves_list.append(raw_curve)
+    if len(curves_list) > max_curves_list_len:
+        curves_list = curves_list[-max_curves_list_len:]
+    curve = int(np.average(curves_list))
+
+    warped_image_inv = utils.warp_image(
+        thresholded_image, points, width, height, inv=True
+    )
+    warped_image_inv[0 : image.shape[0], 0 : image.shape[1]] = (
+        warped_image_inv[0 : image.shape[0], 0 : image.shape[1]] & GREEN
+    )
+    warped_image_inv[0:240, 0 : image.shape[1]] = (
+        warped_image_inv[0:240, 0 : image.shape[1]] & BLACK
+    )
+    result_image = cv2.addWeighted(image_copy, 0.7, warped_image_inv, 1, 0)
+
+    str_curve = str(curve)
+    right_distance = image.shape[1] // 2
+    if len(str_curve) == 3:
+        right_distance = image.shape[1] // 2 - 60
+    elif len(str_curve) == 2:
+        right_distance = image.shape[1] // 2 - 30
+    cv2.putText(
+        result_image,
+        str_curve,
+        (right_distance, 85),
+        cv2.FONT_HERSHEY_COMPLEX_SMALL,
+        2,
+        text_color,
+        3,
+    )
+
+    cv2.rectangle(
+        result_image,
+        (0, image.shape[0] - 14),
+        (image.shape[1], image.shape[0]),
+        text_color,
+        cv2.FILLED,
+    )
+    for x in range(-30, 30):
+        w = image.shape[1] // 20
+        cv2.line(
+            result_image,
+            (w * x + int(curve // 50), image.shape[0] - 13),
+            (w * x + int(curve // 50), image.shape[0] - 1),
+            text_background,
+            2,
+        )
+
+    cv2.line(
+        result_image,
+        (image.shape[1] // 2, image.shape[0] - 6),
+        (image.shape[1] // 2 + (curve * 3), image.shape[0] - 6),
+        corner_color,
+        2,
+    )
+    cv2.line(
+        result_image,
+        ((image.shape[1] // 2 + (curve * 3)), image.shape[0] - 13),
+        (image.shape[1] // 2 + (curve * 3), image.shape[0] - 1),
+        corner_color,
+        6,
+    )
+    cv2.circle(
+        result_image,
+        (image.shape[1] // 2, image.shape[0] - 6),
+        4,
+        corner_color,
+        cv2.FILLED,
+    )
+
+    if show == 2:
+        stacked_images = utils.stack_images(
+            0.95,
+            (
+                [image, image_with_points, warped_image],
+                [summation_image, warped_image_inv, result_image],
+            ),
+        )
+        cv2.imshow("Stacked Images: Pipeline", stacked_images)
+    elif show == 1:
+        cv2.imshow("Result Image", result_image)
+
+    return curve
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(video_path)
+
+    frame_counter = 0
+    while True:
+        _, frame = cap.read()  # 640*480 === 160*120
+        frame = cv2.resize(frame, (480, 360))  # type: ignore
+
+        curve = get_image_curve(frame, show=2)
+        # curve = get_image_curve(frame)
+        print(curve)
+
+        # plt.imshow(frame)
+        # plt.show()
+
+        frame_counter += 1
+        if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            frame_counter = 0
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ Lane_Detection/utils.py
@@ -0,0 +1,132 @@
+import cv2
+import numpy as np
+
+
+def threshold_image(image):
+    lower_white = np.array([70, 0, 0])
+    upper_white = np.array([179, 255, 255])
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
+    thresholded_image = cv2.bitwise_and(image, image, mask=white_mask)
+
+    return thresholded_image
+
+
+def warp_image(image, original_points, width, height, inv=False):
+    pts1 = np.float32(original_points)
+    pts2 = np.float32([[0, 0], [width, 0], [width, height], [0, height]])  # type: ignore
+
+    if inv:
+        matrix = cv2.getPerspectiveTransform(pts2, pts2)  # type: ignore
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)  # type: ignore
+
+    warped_image = cv2.warpPerspective(image, matrix, (width, height))
+
+    return warped_image
+
+
+def draw_points(image, points, color=(0, 0, 255)):
+    for i in range(4):
+        cv2.circle(image, (int(points[i][0]), int(points[i][1])), 6, color, cv2.FILLED)  # type: ignore
+
+    return image
+
+
+def pixel_summation(image, percent=0.1, show=False, region=1):
+    image_copy = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    if region == 1:
+        summation_values = np.sum(image_copy, axis=0)
+    else:
+        summation_values = np.sum(
+            image_copy[image_copy.shape[0] // region :, :], axis=0
+        )
+
+    max_summation = np.max(summation_values)
+    min_summation = percent * max_summation
+
+    indexes_array = np.where(summation_values >= min_summation)
+    base_point = int(np.average(indexes_array))
+
+    if show:
+        summation_image = np.zeros(image.shape, np.uint8)
+        for i, intensity in enumerate(summation_values):
+            cv2.line(
+                summation_image,
+                (i, image.shape[0]),
+                (i, image.shape[0] - (intensity // 200 // region)),
+                (0, 0, 255),
+            )
+
+        cv2.circle(
+            summation_image,
+            (base_point, image.shape[0]),
+            15,
+            (255, 0, 0),
+            cv2.FILLED,
+        )
+
+        return base_point, summation_image
+
+    return base_point, None
+
+
+def stack_images(scale, images_array):
+    rows = len(images_array)
+    cols = len(images_array[0])
+
+    available_rows = isinstance(images_array[0], list)
+    width = images_array[0][0].shape[1]
+    height = images_array[0][0].shape[0]
+
+    if available_rows:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if images_array[x][y].shape[:2] == images_array[0][0].shape[:2]:
+                    images_array[x][y] = cv2.resize(
+                        images_array[x][y], (0, 0), None, scale, scale
+                    )
+                else:
+                    images_array[x][y] = cv2.resize(
+                        images_array[x][y],
+                        (images_array[0][0].shape[1], images_array[0][0].shape[0]),
+                        None,
+                        scale,
+                        scale,
+                    )
+
+                if len(images_array[x][y].shape) == 2:
+                    images_array[x][y] = cv2.cvtColor(
+                        images_array[x][y], cv2.COLOR_GRAY2BGR
+                    )
+
+        blank_image = np.zeros((height, width, 3), np.uint8)
+        hor = [blank_image] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(images_array[x])
+
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if images_array[x].shape[:2] == images_array[0].shape[:2]:
+                images_array[x] = cv2.resize(
+                    images_array[x], (0, 0), None, scale, scale
+                )
+            else:
+                images_array[x] = cv2.resize(
+                    images_array[x],
+                    (images_array[0].shape[1], images_array[0].shape[0]),
+                    None,
+                    scale,
+                    scale,
+                )
+
+            if len(images_array[x].shape) == 2:
+                images_array[x] = cv2.cvtColor(images_array[x], cv2.COLOR_GRAY2BGR)
+
+        hor = np.hstack(images_array)
+        ver = hor
+
+    return ver
