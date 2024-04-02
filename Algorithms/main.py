0e9f598331f09dc5bf3a1f7b4f9d2db2f1a432fb
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-04-02 21:08:17 +0000

    Create the main board pcb schematic (esp32 cam only)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -5,6 +5,7 @@ import random
 def handle_steering_angle_update(steering_angle):
     url = "http://192.168.45.202/"
     params = {"steeringAngle": steering_angle}
+
     response = requests.put(url, params=params)
     if response.status_code == 200:
         print("Steering angle updated successfully")
