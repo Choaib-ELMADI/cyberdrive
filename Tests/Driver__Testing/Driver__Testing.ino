b704667cc64d432f7dcd9568f8325d4790b80277
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-04-04 22:12:12 +0000

    Facing some issues with the dc motors using esp32

--- Tests/Driver__Testing/Driver__Testing.ino
+++ Tests/Driver__Testing/Driver__Testing.ino
@@ -9,6 +9,10 @@ const uint8_t IN3 = 16;
 const uint8_t IN4 = 17;
 
 const int BASE_SPEED = 100;
+const uint8_t CHANNEL_A = 0;
+const uint8_t CHANNEL_B = 1;
+const uint32_t FREQUENCY = 2000;
+const uint8_t RESOLUTION_BITS = 8;
 
 void setup() {
     pinMode(ENA, OUTPUT);
@@ -17,44 +21,44 @@ void setup() {
     pinMode(ENB, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
+
+    ledcSetup(CHANNEL_A, FREQUENCY, RESOLUTION_BITS);
+    ledcAttachPin(ENA, CHANNEL_A);
+    ledcSetup(CHANNEL_B, FREQUENCY, RESOLUTION_BITS);
+    ledcAttachPin(ENB, CHANNEL_B);
 }
 
 void loop() {
-    goForward();
+    goForward(80);
     delay(3000);
-
-    goBackward();
+    goForward(120);
     delay(3000);
-
-    left();
-    delay(1000);
-
-    right();
-    delay(1000);
+    goForward(255);
+    delay(3000);
 }
 
-void goForward() {
+void goForward(uint8_t speed) {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    analogWrite(ENA, BASE_SPEED);
+    ledcWrite(CHANNEL_A, speed);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    analogWrite(ENB, BASE_SPEED);
+    ledcWrite(CHANNEL_B, speed);
 }
 
-void goBackward() {
+void goBackward(uint8_t speed) {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-    analogWrite(ENA, BASE_SPEED);
+    ledcWrite(CHANNEL_A, speed);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
-    analogWrite(ENB, BASE_SPEED);
+    ledcWrite(CHANNEL_B, speed);
 }
 
 void left() {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    analogWrite(ENA, BASE_SPEED);
+    ledcWrite(CHANNEL_A, BASE_SPEED);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
 }
@@ -64,5 +68,5 @@ void right() {
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    analogWrite(ENB, BASE_SPEED);
+    ledcWrite(CHANNEL_B, BASE_SPEED);
 }
