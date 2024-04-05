bf9e38ed133e2a7da3e417699341e0e756a68a64
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-04-05 06:47:30 +0000

    Fix the dc motors drive issues

--- Tests/Driver__Testing__ARD/Driver__Testing__ARD.ino
+++ Tests/Driver__Testing__ARD/Driver__Testing__ARD.ino
@@ -1,18 +1,14 @@
 // Motor A: RIGHT
-const uint8_t ENA = 15;
-const uint8_t IN1 = 2;
-const uint8_t IN2 = 0;
+const uint8_t ENA = 3;
+const uint8_t IN1 = 4;
+const uint8_t IN2 = 5;
 
 // Motor B: LEFT
-const uint8_t ENB = 4;
-const uint8_t IN3 = 16;
-const uint8_t IN4 = 17;
+const uint8_t IN3 = 7;
+const uint8_t IN4 = 6;
+const uint8_t ENB = 9;
 
 const int BASE_SPEED = 100;
-const uint8_t CHANNEL_A = 0;
-const uint8_t CHANNEL_B = 1;
-const uint32_t FREQUENCY = 2000;
-const uint8_t RESOLUTION_BITS = 8;
 
 void setup() {
     pinMode(ENA, OUTPUT);
@@ -21,44 +17,44 @@ void setup() {
     pinMode(ENB, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
-
-    ledcSetup(CHANNEL_A, FREQUENCY, RESOLUTION_BITS);
-    ledcAttachPin(ENA, CHANNEL_A);
-    ledcSetup(CHANNEL_B, FREQUENCY, RESOLUTION_BITS);
-    ledcAttachPin(ENB, CHANNEL_B);
 }
 
 void loop() {
-    goForward(80);
-    delay(3000);
     goForward(120);
     delay(3000);
-    goForward(255);
+
+    goBackward(120);
     delay(3000);
+
+    left();
+    delay(1500);
+
+    right();
+    delay(1500);
 }
 
 void goForward(uint8_t speed) {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    ledcWrite(CHANNEL_A, speed);
+    analogWrite(ENA, speed);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    ledcWrite(CHANNEL_B, speed);
+    analogWrite(ENB, speed);
 }
 
 void goBackward(uint8_t speed) {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-    ledcWrite(CHANNEL_A, speed);
+    analogWrite(ENA, speed);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
-    ledcWrite(CHANNEL_B, speed);
+    analogWrite(ENB, speed);
 }
 
 void left() {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    ledcWrite(CHANNEL_A, BASE_SPEED);
+    analogWrite(ENA, BASE_SPEED);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
 }
@@ -68,5 +64,5 @@ void right() {
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    ledcWrite(CHANNEL_B, BASE_SPEED);
+    analogWrite(ENB, BASE_SPEED);
 }
---
+++ Tests/Driver__Testing__ESP/Driver__Testing__ESP.ino
@@ -0,0 +1,77 @@
+// Motor A: RIGHT
+const uint8_t ENA = 15;
+const uint8_t IN1 = 2;
+const uint8_t IN2 = 0;
+
+// Motor B: LEFT
+const uint8_t IN3 = 16;
+const uint8_t IN4 = 4;
+const uint8_t ENB = 17;
+
+const int BASE_SPEED = 100;
+const uint8_t CHANNEL_A = 0;
+const uint8_t CHANNEL_B = 1;
+const uint32_t FREQUENCY = 2000;
+const uint8_t RESOLUTION_BITS = 8;
+
+void setup() {
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+    ledcSetup(CHANNEL_A, FREQUENCY, RESOLUTION_BITS);
+    ledcAttachPin(ENA, CHANNEL_A);
+    ledcSetup(CHANNEL_B, FREQUENCY, RESOLUTION_BITS);
+    ledcAttachPin(ENB, CHANNEL_B);
+}
+
+void loop() {
+    goForward(120);
+    delay(3000);
+
+    goBackward(120);
+    delay(3000);
+
+    left();
+    delay(1500);
+
+    right();
+    delay(1500);
+}
+
+void goForward(uint8_t speed) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    ledcWrite(CHANNEL_A, speed);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    ledcWrite(CHANNEL_B, speed);
+}
+
+void goBackward(uint8_t speed) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    ledcWrite(CHANNEL_A, speed);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    ledcWrite(CHANNEL_B, speed);
+}
+
+void left() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    ledcWrite(CHANNEL_A, BASE_SPEED);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
+
+void right() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    ledcWrite(CHANNEL_B, BASE_SPEED);
+}
